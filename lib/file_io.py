97c3338463da0dbfed2fce76843eeb88b0220a94
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-05-29 10:23:15 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
-
+    file_path = Path(file_name).with_suffix('.txt')
+    with file_path.open(mode='w') as file:
+        file.write(file_content)
 def append_file(file_name, append_content):
-    pass
-
+    file_path = Path(file_name).with_suffix('.txt')
+    with file_path.open(mode='a') as file:
+        file.write(append_content)
 def read_file(file_name):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with file_path.open(mode='r') as file:
+        content = file.read()
+    return content
+
+
+    
